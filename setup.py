9d0af79efa750e0d3a2d45baaca86d793a0ce879
titusz <tp@py7.de>
2013-07-26 19:00:15 +0200

    bump version to 0.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 NEWS = open(os.path.join(here, 'NEWS.txt')).read()
 
-version = '0.1'
+version = '0.2'
 
 setup(
     name='ibooks',
